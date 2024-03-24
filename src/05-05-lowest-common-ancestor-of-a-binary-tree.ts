e2db68e7f12dd604e424fa3155ede79d8b69f13f
Francisco Moretti <franciscoemoretti@gmail.com>
2024-03-24 10:46:09 +0000

    lowest common and moved some problems

---
+++ src/05-05-lowest-common-ancestor-of-a-binary-tree.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(
+  root: TreeNode | null,
+  p: TreeNode | null,
+  q: TreeNode | null
+): TreeNode | null {
+  if (!root || root == p || root == q) {
+    return root; // null, p or q
+  }
+
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+
+  if (left && right) {
+    return root; // Found the common ancestor
+  } else if (left) {
+    return left; // Propagate the found node on the left
+  } else if (right) {
+    return right; // Propagate the found node on the right
+  } else {
+    return null;
+  }
+}
