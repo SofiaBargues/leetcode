27eb5dd5f6e0bd26db3e9eab7a05c5bb7531dd45
Francisco Moretti <franciscoemoretti@gmail.com>
2024-03-25 13:45:03 +0000

    word break dp tb

--- python/06-01-word-break.py
+++ python/06-01-word-break.py
@@ -1,7 +1,26 @@
 from itertools import chain
 
 
+class SolutionDpTopBottom:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # Should do DP
+        validLocations = [False]*(len(s)+1)
 
+        validLocations[len(s)] = True
+
+        for i in range(len(s)-1, -1, -1):
+            for word in wordDict:
+                wordEndIdx = len(word) + i
+                if len(word) + i > len(s):
+                    continue
+                if not validLocations[wordEndIdx]:
+                    continue
+
+                if word == s[i:wordEndIdx]:
+                    validLocations[i] = True
+                    break
+
+        return validLocations[0]
 
 class SolutionDpBottomUp:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
