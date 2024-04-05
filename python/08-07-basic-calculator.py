bd722fafb1a496debb479314050b7bf8f4fcc9c2
Francisco Moretti <franciscoemoretti@gmail.com>
2024-04-05 10:02:18 +0100

    other sol

--- python/08-07-basic-calculator.py
+++ python/08-07-basic-calculator.py
@@ -1,4 +1,26 @@
 class Solution:
+  def calculate(self, s: str) -> int:
+    ans = 0
+    num = 0
+    sign = 1
+    stack = [sign]  # stack[-1]: current env's sign
+
+    for c in s:
+      if c.isdigit():
+        num = num * 10 + (ord(c) - ord('0'))
+      elif c == '(':
+        stack.append(sign)
+      elif c == ')':
+        stack.pop()
+      elif c == '+' or c == '-':
+        ans += sign * num
+        sign = (1 if c == '+' else -1) * stack[-1]
+        num = 0
+
+    return ans + sign * num
+
+
+class SolutionRaw:
 
     def operate(self, num1, operator, num2):
         if operator == '+':
