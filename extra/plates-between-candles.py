27b02e900e6e5ebe95b2f38d12fad2cb4e5d9053
Francisco Moretti <franciscoemoretti@gmail.com>
2024-04-05 09:50:50 +0100

    platges candles

--- extra/plates-between-candles.py
+++ extra/plates-between-candles.py
@@ -1,4 +1,19 @@
+from bisect import bisect, bisect_left
+
 class Solution:
+    def platesBetweenCandlesBinarySearch(self, s, queries):
+        candle_idxs = [i for i, char in enumerate(s) if char == '|'] # candles indexes
+        # C = [2, 5, 9]
+        res = []
+        for start, end in queries:
+            i = bisect.bisect_left(candle_idxs, start) # 0
+            j = bisect.bisect(candle_idxs, end) - 1 # 1
+            inner_candles = j - i
+            total_positions = candle_idxs[j] - candle_idxs[i]
+            res.append((total_positions) - (inner_candles) if i < j else 0)
+        return res
+
+
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
         # Build candles prefix sum
         prefixCandles = []
