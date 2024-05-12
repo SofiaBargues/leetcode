41161a3eb7121431db48722f77a5e5bd24fbbd2f
Francisco Moretti <franciscoemoretti@gmail.com>
2024-05-12 16:31:30 +0100

    comments

--- leetcode/0011-container-with-most-water.py
+++ leetcode/0011-container-with-most-water.py
@@ -1,16 +1,22 @@
+from typing import List
+
+
 class Solution:
+    # Time: O(n)
+    # Space: O(1)
+    # https://leetcode.com/problems/container-with-most-water/description/
     def maxArea(self, height: List[int]) -> int:
         l = 0
-        r = len(height)-1
+        r = len(height) - 1
         maxArea = 0
-        while l<r:
+        while l < r:
             # calculate area and save
-            currArea = (r-l)*min(height[l], height[r])
+            currArea = (r - l) * min(height[l], height[r])
             maxArea = max(currArea, maxArea)
             # Move one pointer
             if height[l] > height[r]:
-                r-=1
+                r -= 1
             else:
-                l+=1
+                l += 1
 
         return maxArea
