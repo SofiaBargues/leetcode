12069489839346402c5768cb290f6e5dc7005871
Francisco Moretti <franciscoemoretti@gmail.com>
2024-06-05 10:10:06 +0100

    sort list

--- leetcode/0148-sort-list.py
+++ leetcode/0148-sort-list.py
@@ -6,6 +6,50 @@
 from collections import defaultdict
 
 
+# There is a  Time: O(nlogn) Space: O(n) solution but its too complex
+class SolutionMergeSort:
+    # Time: O(nlogn) Space: O(logn)
+
+    def getMid(self, head: ListNode):
+        slow = fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        # TODO: check OFf by 1?
+        return slow
+
+    def merge(self, left, right):
+        dummy = tail = ListNode()
+        while left and right:
+            if left.val < right.val:
+                tail.next = left
+                left = left.next
+            else:
+                tail.next = right
+                right = right.next
+            tail = tail.next
+
+        if left:
+            tail.next = left
+        if right:
+            tail.next = right
+        return dummy.next
+
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        mid = self.getMid(head)
+        right = mid.next
+        # Split lists
+        mid.next = None
+
+        left = self.sortList(head)
+        right = self.sortList(right)
+
+        head = self.merge(left, right)
+        return head
+
+
 class SolutionNonOptimal:
     # Time: O(nlogn) Space: O(n)
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
