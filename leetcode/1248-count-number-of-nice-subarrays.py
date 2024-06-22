3bc3b979d134bb5b1de1b621af652db4c16212b7
Francisco Moretti <franciscoemoretti@gmail.com>
2024-06-22 09:36:13 +0100

    unused

--- leetcode/1248-count-number-of-nice-subarrays.py
+++ leetcode/1248-count-number-of-nice-subarrays.py
@@ -1,7 +1,6 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         nums = [num % 2 for num in nums]
-        prefix = []
 
         prefix_count = [0] * (len(nums)+1)
         prefix_count[0] = 1
@@ -10,7 +9,7 @@ class Solution:
         subs = 0
 
         for num in nums:
-            curr+=num 
+            curr+=num
             if curr >= k:
                 subs += prefix_count[curr-k]
             prefix_count[curr]+=1
