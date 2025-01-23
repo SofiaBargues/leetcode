083972c74465f7cbdd9571309a7f8cdc193ddd6b
Sofia Bargues <barguessofia@gmail.com>
2025-01-23 09:50:18 +0000

    format

--- src/0101-symmetric-tree.js
+++ src/0101-symmetric-tree.js
@@ -11,17 +11,22 @@
  * @return {boolean}
  */
 var isSymmetric = function (root) {
-    let root1 = root.left
-    let root2 = root.right
-    function helper(root1, root2) {
-        //base
-        if (root1 === null && root2 === null) { return true }
-        if (root1 === null || root2 === null) { return false }
-        //node work
-        if (root1.val != root2.val) { return false }
-        //recurtion 
-        return helper(root1.left, root2.right) && helper(root2.left, root1.right)
-
+  let root1 = root.left;
+  let root2 = root.right;
+  function helper(root1, root2) {
+    //base
+    if (root1 === null && root2 === null) {
+      return true;
     }
-   return helper(root1, root2)
+    if (root1 === null || root2 === null) {
+      return false;
+    }
+    //node work
+    if (root1.val != root2.val) {
+      return false;
+    }
+    //recurtion
+    return helper(root1.left, root2.right) && helper(root2.left, root1.right);
+  }
+  return helper(root1, root2);
 };
