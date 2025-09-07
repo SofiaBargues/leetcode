89cfaeac1b292b645be324c39533b219ad458fb5
Sofia Bargues <barguessofia@gmail.com>
2025-09-07 20:35:14 +0100

    some day

--- src/1304-find-n-unique-integers-sum-up-to-zero.js
+++ src/1304-find-n-unique-integers-sum-up-to-zero.js
@@ -15,3 +15,25 @@ var sumZero = function (n) {
     arr[n - 1] = -total
     return arr
 };
+
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var sumZero = function (n) {
+    
+    let result = []
+
+    if (n % 2 != 0) {
+        result.push(0)
+        n--
+    }
+
+    for (let i = 1; i <= n / 2; i++) {
+        result.push(-i)
+        result.push(i)
+    }
+
+    return result
+};
