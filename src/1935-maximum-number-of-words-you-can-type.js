dce2df1fde95eb8e5cbecc5ce6bb4d1b1c5aa7f8
Sofia Bargues <barguessofia@gmail.com>
2025-09-15 20:04:50 +0100

    SDXFASDFA

--- src/1935-maximum-number-of-words-you-can-type.js
+++ src/1935-maximum-number-of-words-you-can-type.js
@@ -24,3 +24,29 @@ var canBeTypedWords = function (text, brokenLetters) {
     }
     return result
 };
+
+/**
+ * @param {string} text
+ * @param {string} brokenLetters
+ * @return {number}
+ */
+var canBeTypedWords = function (text, brokenLetters) {
+
+    let arrText = text.split(" ")
+    let arrBrokenLetters = brokenLetters.split("")
+    let total = 0
+    if (arrBrokenLetters.length===0) return arrText.length
+
+    for (const word of arrText) {
+        for (let i = 0; i <= arrBrokenLetters.length - 1; i++) {
+            let brokenKey = arrBrokenLetters[i]
+            if (word.indexOf(brokenKey) != -1) {
+                break
+            } else if ( i === arrBrokenLetters.length- 1) total++
+        }
+
+    }
+    return total
+};
+
+
