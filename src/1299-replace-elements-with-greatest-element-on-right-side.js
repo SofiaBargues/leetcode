55d7e24f32b764f857fd2909845e935ef8867746
Sofia Bargues <barguessofia@gmail.com>
2025-10-04 17:38:20 +0100

    day 27?

---
+++ src/1299-replace-elements-with-greatest-element-on-right-side.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var replaceElements = function (arr) {
+    let result = [...arr]
+    let max= arr[arr.length-1]
+    for (let i = arr.length-2 ; i >= 0; i--) {
+        result[i] = max
+        if (arr[i]>max)max=arr[i]
+    }
+    result[arr.length - 1] = -1
+    return result
+};
+
