64e49053c047c38cdd3e24ab0480ce1a66821000
Sofia Bargues <barguessofia@gmail.com>
2025-10-14 07:36:08 +0100

    dau 287

--- src/3349-adjacent-increasing-subarrays-detection-i.js
+++ src/3349-adjacent-increasing-subarrays-detection-i.js
@@ -30,3 +30,29 @@ var hasIncreasingSubarrays = function (nums, k) {
     }
     return false
 };
+
+
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var hasIncreasingSubarrays = function (nums, k) {
+    if (k===1){
+        return nums.length>=2
+    }
+    let total = 1
+    for (let i = 0; i <= nums.length - 2; i++) {
+        let b = k + i
+        if (nums[i] < nums[i + 1] && nums[b] < nums[b + 1]) {
+            total++
+            if (total === k) {
+                return true
+            }
+        } else {
+            total =1}
+    }
+    return false
+};
