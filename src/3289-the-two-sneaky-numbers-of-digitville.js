60619d515eb3922091b3100363de7491dd24f099
Sofia Bargues <barguessofia@gmail.com>
2025-10-31 20:00:48 +0000

    day304

---
+++ src/3289-the-two-sneaky-numbers-of-digitville.js
@@ -0,0 +1,15 @@
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getSneakyNumbers = function (nums) {
+    let set = new Set([])
+    let result = []
+    for (const num of nums) {
+        if (set.has(num)) result.push(num)
+        else set.add(num)
+    }
+    return result
+};
