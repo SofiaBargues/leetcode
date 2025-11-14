c8ece5b0755fe85a59e3538dc384db4b4317ca2c
Sofia Bargues <barguessofia@gmail.com>
2025-11-14 19:32:13 +0000

    sd

---
+++ src/0504-base-7.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function (num) {
+    if (num === 0) return "0"
+    const sign = num > 0 ? "" : "-"
+     num = Math.abs(num)
+    let res = ""
+    while (num > 0) {
+        res = (num % 7) + res
+        num = Math.floor(num / 7)
+    }
+    return sign + res
+};
