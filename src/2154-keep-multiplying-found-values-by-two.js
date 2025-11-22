4a9305f8370ac97f97cb9a7b05b72c77989414c6
Sofia Bargues <barguessofia@gmail.com>
2025-11-22 08:31:03 +0000

    szdfcx

--- src/2154-keep-multiplying-found-values-by-two.js
+++ src/2154-keep-multiplying-found-values-by-two.js
@@ -8,3 +8,15 @@ var findFinalValue = function (nums, original) {
     return original
 };
 
+/**
+ * @param {number[]} nums
+ * @param {number} original
+ * @return {number}
+ */
+var findFinalValue = function (nums, original) {
+
+    const set = new Set(nums)
+    while (set.has(original)) original = original * 2
+    return original
+
+};
