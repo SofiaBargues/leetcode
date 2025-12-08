e8551145b9e4e66db28843ec4b1d161951232b02
Sofia Bargues <barguessofia@gmail.com>
2025-12-08 07:47:15 +0000

    dfg

--- src/1523-count-odd-numbers-in-an-interval-range.js
+++ src/1523-count-odd-numbers-in-an-interval-range.js
@@ -5,20 +5,33 @@
  */
 
 var countOdds = function (low, high) {
-    function isOdd(num) {
-        if (num % 2 === 0) return false
-        return true
-    }
-    let L = high - low + 1
-    if (!isOdd(L)) {
-        return L / 2
+  if (low % 2 === 0) low++;
+  if (high % 2 === 0) high--;
+
+  return Math.ceil((high - low + 1) / 2);
+};
+
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+
+var countOdds = function (low, high) {
+  function isOdd(num) {
+    if (num % 2 === 0) return false;
+    return true;
+  }
+  let L = high - low + 1;
+  if (!isOdd(L)) {
+    return L / 2;
+  } else {
+    if (isOdd(low)) {
+      return Math.ceil(L / 2);
     } else {
-        if (isOdd(low)) {
-            return Math.ceil(L / 2)
-        } else {
-            return Math.floor(L / 2)
-        }
+      return Math.floor(L / 2);
     }
+  }
 };
 
 // var countOdds = function (low, high) {
